511af537536c4f6068d3d90237c813f152223b11
tranchau <tranchau1991@gmail.com>
2015-10-29 16:03:04 +0700

    update

--- app/src/main/java/com/example/jtec/show_html_sdcard/MainActivity.java
+++ app/src/main/java/com/example/jtec/show_html_sdcard/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -13,24 +14,54 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void btn_load(View view){
-        WebView webView = (WebView)findViewById(R.id.webView);
+        webView = (WebView)findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url);
+                return true; // then it is not handled by default action
+            }
+        });
+        webView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+    }
+    //back for webview
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    public void btn_load(View view){
+
 
         if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
             Log.d("Error", "No SDCARD");
         } else {
             EditText editText = (EditText)findViewById(R.id.editText);
-            //default file:///sdcard/a1/chuong_1/bai1.html
+            //default file:///sdcard/a1/menu.html
             String value = editText.getText().toString();
             webView.loadUrl(value);
         }
